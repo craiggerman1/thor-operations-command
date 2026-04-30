bd1c80df9cf52c4849e9a76a0c0f5c366606a6f8
Craig German <craig@thormobile.com.au>
2026-04-30 22:01:15 +1000

    Raise floating todo panel

--- src/components/TocShell.tsx
+++ src/components/TocShell.tsx
@@ -124,7 +124,7 @@ export function TocShell({ children }: { children: ReactNode }) {
             <div className="build-notice" aria-label="Beta testing and build version">
               <strong>Beta</strong>
               <span>Not for internal operational use</span>
-              <em>Build 0.081</em>
+              <em>Build 0.082</em>
             </div>
           </div>
           <div className="topbar-actions">
