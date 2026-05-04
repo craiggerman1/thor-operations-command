55af0ee5f61aff33f4e1e77aac6d32377be99445
Craig German <craig@thormobile.com.au>
2026-05-04 22:47:03 +1000

    Refine staff availability board

--- src/components/TocShell.tsx
+++ src/components/TocShell.tsx
@@ -287,7 +287,7 @@ export function TocShell({ children }: { children: ReactNode }) {
             </div>
             <div className="build-notice" aria-label="Beta testing and build version">
               <strong>BETA</strong>
-              <em>Build 0.162</em>
+              <em>Build 0.163</em>
               <span className="units-counter"><b>{unitsWashedToday}</b> units washed today</span>
             </div>
           </div>
