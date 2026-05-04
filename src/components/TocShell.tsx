7889e81f67227ad6d3b54943d2537c1126758bab
Craig German <craig@thormobile.com.au>
2026-05-04 22:51:36 +1000

    Fix staff availability header cell

--- src/components/TocShell.tsx
+++ src/components/TocShell.tsx
@@ -287,7 +287,7 @@ export function TocShell({ children }: { children: ReactNode }) {
             </div>
             <div className="build-notice" aria-label="Beta testing and build version">
               <strong>BETA</strong>
-              <em>Build 0.163</em>
+              <em>Build 0.164</em>
               <span className="units-counter"><b>{unitsWashedToday}</b> units washed today</span>
             </div>
           </div>
