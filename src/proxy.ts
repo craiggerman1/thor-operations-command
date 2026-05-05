a6527a8dc252e22ebfbf27c8e812b09968d981ae
Craig German <craig@thormobile.com.au>
2026-05-05 12:27:26 +1000

    Rename middleware to proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 const REALM = "Thor Operations Command";
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   const sitePassword = process.env.TOC_SITE_PASSWORD;
 
   if (!sitePassword) {
