bf952be4ee7f8a45fb0d7a10a0780e89023ee02b
Craig German <craig@thormobile.com.au>
2026-05-11 23:30:53 +1000

    Grant setup access to operations tables

---
+++ supabase/migrations/202605110007_grant_service_role_operations_setup_tables.sql
@@ -0,0 +1,4 @@
+grant all on table public.operation_sites to service_role;
+grant all on table public.site_schedules to service_role;
+grant all on table public.staff_induction_cache to service_role;
+grant all on table public.calendar_job_staff to service_role;
