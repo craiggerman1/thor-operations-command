1fffbb380160c5dac6c3fff65a3b2bd6e989d122
Craig German <craig@thormobile.com.au>
2026-05-12 19:02:07 +1000

    Grant availability cache service role access

---
+++ supabase/migrations/202605120001_grant_staff_availability_cache_service_role.sql
@@ -0,0 +1,2 @@
+grant select, insert, update, delete on table public.staff_availability_cache to service_role;
+grant select, insert, update, delete on table public.staff_induction_cache to service_role;
